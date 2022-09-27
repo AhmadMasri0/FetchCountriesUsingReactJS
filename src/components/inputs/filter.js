84abedacf64e87444282ff4895100e8a6ffd5905
temo-sudo <you@example.com>
2022-09-27 18:36:59 +0300

    Editin the Filter's label

--- src/components/inputs/filter.js
+++ src/components/inputs/filter.js
@@ -5,9 +5,9 @@ const regions = ['Asia', 'Africa', 'America', 'Europe', 'Oceania'];
 export default function Filter({select}) {
     return <FormControl>
         <InputLabel id="filter-label">Filter by region</InputLabel>
-        <Select labelId="filter-label" className={select}>
+        <Select labelId="filter-label" label="Filter by region"  className={select}>
             {regions.map((region) => (
-                <MenuItem key={region}>
+                <MenuItem key={region} value={region}>
                     {region}
                 </MenuItem>
             ))}
